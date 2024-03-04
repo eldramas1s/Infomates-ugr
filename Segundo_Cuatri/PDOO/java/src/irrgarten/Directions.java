b62487927c9bfb915b4355d8644bea9e511f989f
FLL2016 <airamfalconmarrero@gmail.com>
2024-03-04 18:00:56 +0100

    Inclusión irrgarten

---
+++ Segundo_Cuatri/PDOO/java/src/irrgarten/Directions.java
@@ -0,0 +1,14 @@
+package irrgarten;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author airam
+ */
+public enum Directions {
+    LEFT, RIGHT, UP, DOWN
+}
---
+++ Segundo_Cuatri/PDOO/java/src/irrgarten/Weapon.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author airam
+ */
+public class Weapon {
+    private float power;
+    private int uses;
+    
+    Weapon(float strength, int durability){
+        power = strength;
+        uses = durability;
+    }
+    
+    public float Attack(){  
+        float atk_val = 0;
+        if(uses > 0){
+            atk_val = power;
+            uses--;
+        }
+        return atk_val;
+    }
+    
+    public String toString(){
+        return "W[" + power +"," + uses + "]";
+    }
+    
+}
+
