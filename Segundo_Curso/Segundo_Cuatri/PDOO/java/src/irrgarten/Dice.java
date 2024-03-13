29d6750f8a738d4738200ab413bb3bae14e0bcd4
FLL2016 <airamfalconmarrero@gmail.com>
2024-03-13 18:29:41 +0100

    Java: Hecha la documentacion e implementado el método discard (que pensaba que estaba ya)

--- Segundo_Curso/Segundo_Cuatri/PDOO/java/src/irrgarten/Dice.java
+++ Segundo_Curso/Segundo_Cuatri/PDOO/java/src/irrgarten/Dice.java
@@ -14,69 +14,160 @@ import java.util.Random;
  */
 public class Dice { 
     
-    static private int MAX_USES = 5; //numero máximo de usos de armas y escudos
-    static private float MAX_INTELLIGENCE = 10.0f; //Valor máximo para la inteligencia de jugadores y monstruos
-    static private float MAX_STRENGTH =10.0f; //Valor máximo fuerza jugadores
-    static private float RESURRECT_PROB=0.3f; //probabilidad de ser resucitado en cada turno
-    static private int WEAPONS_REWARD=2; //máximo de armas ganadas por combate ganado
-    static private int SHIELDS_REWARD=3;//máximo de escudos ganados por combate ganado
-    static private int HEALTH_REWARD=5;//maximo de vida recuperada por combate ganado
-    static private int MAX_ATTACK=3;//máxima potencia de las armas
-    static private int MAX_SHIELD=2;//máximo potencia de los escudos
     
+    
+    /**
+     *  Numero máximo de usos de armas y escudos
+     */
+    static private int MAX_USES = 5; 
+    /**
+     *  Valor máximo para la inteligencia de jugadores y monstruos
+     */
+    static private float MAX_INTELLIGENCE = 10.0f; 
+    /**
+     *  Valor máximo fuerza jugadores
+     */
+    static private float MAX_STRENGTH =10.0f; 
+    /**
+     *  Probabilidad de ser resucitado en cada turno
+     */
+    static private float RESURRECT_PROB=0.3f; 
+    /**
+     *  Máximo de armas ganadas por combate ganado
+     */
+    static private int WEAPONS_REWARD=2; 
+    /**
+     *  Máximo de escudos ganados por combate ganado
+     */
+    static private int SHIELDS_REWARD=3;
+    /**
+     *  Maximo de vida recuperada por combate ganado
+     */
+    static private int HEALTH_REWARD=5;
+    /**
+     *  Máxima potencia de las armas
+     */
+    static private int MAX_ATTACK=3;
+    /**
+     *  Máximo potencia de los escudos
+     */
+    static private int MAX_SHIELD=2;
+    
+    /**
+     *  Generador de números aleatorios
+     */
     static Random generator;
-    
+
+
+    /**
+     * Genera la posición relativa a la fila o columna inicial
+     * @param max Cota superior del rango de valores a devolver, no inclusive.
+     * @return El índice de la fila o columna
+     */
     static public int randomPos(int max){
         return generator.nextInt(max);
     }
     
+    /**
+     * Decide qué jugador tendrá el primer turno
+     * @param nPlayers Número de jugadores seleccionables
+     * @return El índice del jugador elegido 
+     */
     static public int whoStarts(int nPlayers){
         return generator.nextInt(nPlayers);
     }
     
+    /**
+     * Genera un valor aleatorio de inteligencia en el intervalo [0,MAX_INTELLIGENCE[
+     * @return valor de inteligencia
+     */
     static public float randomIntelligence(){
         return generator.nextFloat()*MAX_INTELLIGENCE;
     }
     
+    /**
+     * Genera un valor aleatorio de fuerza en el intervalo [0,MAX_STRENGTH[
+     * @return valor de fuerza
+     */
     static public float randomStrength(){
         return generator.nextFloat()*MAX_STRENGTH;
     }
     
+    /**
+     * Decide si un jugador fallecido debe resucitar en el turno actual
+     * @return true si debe resucitar, false en caso contrario
+     */
     static public boolean resurrectPlayer(){
         //Consideramos true como menor para ajustar bien los rangos
         return generator.nextFloat() < RESURRECT_PROB; 
     }
     
+    /**
+     * Decide la cantidad de armas ganadas tras un combate
+     * @return el número de armas en el rango [0,WEAPONS_REWARD]
+     */
     static public int weaponsReward(){
         //+1 para ajustar rango
         return generator.nextInt(WEAPONS_REWARD+1);
     }
     
+    /**
+     * Decide la cantidad de escudos ganadas tras un combate
+     * @return el número de escudos en el rango [0,SHIELDS_REWARD]
+     */
     static public int shieldsReward(){
         //+1 para ajustar rango
         return generator.nextInt(SHIELDS_REWARD+1);
     }
     
+    /**
+     * Decide la cantidad de vida recuperada tras un combate
+     * @return La vida recuperada en el rango [0,HEALTH_REWARD]
+     */
     static public int healthReward(){
         return generator.nextInt(HEALTH_REWARD+1);
     }
     
+    //TODO: cuando sepas que hace expandir comentario
+    /**
+     * Decide el poder de un arma 
+     * @return El poder del arma
+     */
     static public float weaponPower(){
         return generator.nextFloat()*MAX_ATTACK;
     }
 
+    /**
+     * Decide la defensa de un escudo 
+     * @return La defensa del arma
+     */
     static public float shieldPower(){
         return generator.nextFloat()*MAX_SHIELD;
     }
 
+    /**
+     * Decide los usos restantes que tendrá un arma o escudo
+     * @return Los usos restantes
+     */
     static public int usesLeft(){
         return generator.nextInt(MAX_USES+1);
     }
 
+    /**
+     * Devuelve la cantidad de competencia aplicada con un valor aleatorio 
+     * en el rango [0,competence[
+     * @param competence La competencia
+     * @return La competencia generada
+     */
     static public float intensity(float competence){
         return generator.nextFloat() * competence;
     }
 
+    /**
+     * Decide si desechar un arma según el número de usos restantes
+     * @param usesLeft Número de usos del objeto
+     * @return True si decide tirar el arma, false en el caso contrario
+     */
     static public boolean discardElement(int usesLeft){
         //distribuye de forma uniforme la probabilidad
         return generator.nextFloat() < (MAX_USES-usesLeft)/(float) MAX_USES;
--- Segundo_Curso/Segundo_Cuatri/PDOO/java/src/irrgarten/Shield.java
+++ Segundo_Curso/Segundo_Cuatri/PDOO/java/src/irrgarten/Shield.java
@@ -9,14 +9,32 @@ package irrgarten;
  * @author el_dramas
  */
 public class Shield {
+    /**
+     * @brief Atributo que representa de cuanto daño te puedes 
+     * proteger con el escudo
+     */
     private float protection;
+    /**
+     * @brief Atributo que representa cuantos usos quedan hasta
+     * que tu escudo quede inservible.
+     */
     private int uses;
     
+    /**
+     * @brief Constructor, construye un escudo nuevo.
+     * @param defense Defensa del escudo
+     * @param durability Usos que le quedan al escudo
+     */
     Shield (float defense, int durability ){
         protection = defense;
         uses = durability;
     }
     
+    /**
+     * Proteje al jugador que porte el escudo si no está roto,
+     * disminuyendo la vida útil del escudo en uno.
+     * @return El valor de la absorción de daño del escudo
+     */
     public float protect(){
         float prt_val=0;
         if(uses>0){
@@ -25,8 +43,19 @@ public class Shield {
         }
         return prt_val;
     }
-
+    /**
+     * @brief Representa la clase como string
+     * @return Los atributos del escudo
+     */
     public String toString(){
         return "S[" + protection + "," + uses + "]";
+    }
+
+    /**
+     * Decide si desechar un arma
+     * @return True si la desecha
+     */
+    public boolean discard(){
+        return Dice.discardElement(uses);
     }
 }
