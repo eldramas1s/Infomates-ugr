5075121a34287dcbfcfd4319e5de197e8e3795f9
el_dramas <lucashidalgoherrera@gmail.com>
2025-05-17 21:49:34 +0200

    updating

--- Tercer_Curso/Segundo_Cuatri/Metodos_Numericos_II/Practicas/P2/integracion_numerica.py
+++ Tercer_Curso/Segundo_Cuatri/Metodos_Numericos_II/Practicas/P2/integracion_numerica.py
@@ -31,3 +31,53 @@ def formula_rectangulo_medio(f,a,b,n):
     for i in range(1, len(nodos)):
         sum+=f((nodos[i-1]+nodos[i])/2)
     return h*sum
+
+def Simpson_compuesta(f,a,b,n):
+    """ fómula compuesta de Simpson
+        Parametros:
+        f: función a integrar
+        a: límite inferior
+        b: límite superior
+        n: número de subintervalos
+        Devuelve:
+        resultado de la integración
+    """
+    
+    h=(b-a)/n
+    
+    suma=f(a)+f(b)
+    
+    # Generaremos 2(n+1) nodos para la integración simulando ya que se han tomado los puntos medios.
+    
+    x_values = np.linspace(a, b, n+1)  # Genera los nodos de integración
+    
+    for i in range(1, len(x_values)-1):
+        if i%2==0:
+            suma+=2*f(x_values[i])
+        else:
+            suma+=4*f(x_values[i])
+    return (h/3)*suma
+
+def Trapecios_compuesta(f,a,b,n):
+    """fórmula compuesta de los trapecios
+        Parametros:
+        f: función a integrar
+        a: límite inferior
+        b: límite superior
+        n: número de subintervalos
+        Devuelve:
+        resultado de la integración
+    """
+    
+    h=(b-a)/n
+    
+    suma=f(a)+f(b)
+    
+    # Generaremos 2(n+1) nodos para la integración simulando ya que se han tomado los puntos medios.
+    
+    x_values = np.linspace(a, b, n+1)  # Genera los nodos de integración
+    
+    for i in range(1, len(x_values)-1):
+        suma+=2*f(x_values[i])
+    return (h/2)*suma
+
