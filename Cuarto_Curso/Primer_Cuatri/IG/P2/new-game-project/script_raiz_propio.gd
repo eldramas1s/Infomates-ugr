3a4ddf3a8bd3f386f5b157a97fd30363b7314a16
el_dramas <lucashidalgoherrera@gmail.com>
2025-09-30 16:12:23 +0200

    manual commit

---
+++ Cuarto_Curso/Primer_Cuatri/IG/P2/new-game-project/script_raiz_propio.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var dibujar_aristas : bool = false
+
+func _init():
+	RenderingServer.set_debug_generate_wireframes(true)
+	
+func _unhandled_key_input(key_event):
+	if key_event.keycode == KEY_W and not key_event.pressed :
+		dibujar_aristas = not dibujar_aristas
+		var viewport = get_viewport()
+		
+		if dibujar_aristas:
+			viewport.debug_draw = Viewport.DEBUG_DRAW_WIREFRAME
+			print("Dibujar en modo aristas: activado")
+		else:
+			viewport.debug_draw = Viewport.DEBUG_DRAW_DISABLED
+			print("Dibujar en modo aristas: desactivado")
