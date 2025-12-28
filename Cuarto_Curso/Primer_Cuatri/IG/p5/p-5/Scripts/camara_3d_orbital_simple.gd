8a326707c815e84696b41f61124fa307007daab6
el_dramas <lucashidalgoherrera@gmail.com>
2025-12-28 21:45:20 +0100

    updating

--- Cuarto_Curso/Primer_Cuatri/IG/p5/p-5/Scripts/camara_3d_orbital_simple.gd
+++ Cuarto_Curso/Primer_Cuatri/IG/p5/p-5/Scripts/camara_3d_orbital_simple.gd
@@ -1,5 +1,10 @@
 extends Camera3D
 
+## VARIABLES GLOBALES
+var selected_figure = null	## determina la figura que hay seleccionada
+var figures = ["Plano","Cilindro","Cubo"] ## mallas que podemos seleccionar
+var current_figure_index = 0 ## Indices de la figura seleccionada
+
 ## ----------------------------------------------------------------------
 ## contantes y variables de instancia 
 
@@ -23,48 +28,85 @@ func _actualiza_transf_vista(  ) -> void :
 ## -------------------------------------------------------------------
 func _ready() -> void :  ## se ejecuta una vez al inicio
 	_actualiza_transf_vista() ## inicializa la vista al inicio
-	
-## -------------------------------------------------------------------
-## procesa evento de entrada
-
-func _input( event : InputEvent ): 
-	
-	var av : bool = true ## actualizar vista sí/no
-	
-	if event is InputEventKey and event.pressed: ## teclas
-		match event.keycode:
-			KEY_UP:    
-				dxy += Vector2( 0, -at )
-			KEY_DOWN:  
-				dxy += Vector2( 0, +at )
-			KEY_RIGHT: 
-				dxy += Vector2( -at, 0 )
-			KEY_LEFT:
-				dxy += Vector2( at, 0 )
-			KEY_MINUS, KEY_PAGEDOWN, KEY_KP_SUBTRACT: 
-				dz *= 1.05 
-			KEY_PLUS, KEY_PAGEUP, KEY_KP_ADD: 
-				dz = max( dz/1.05, 0.1 )
-			_: 
-				av = false
-				
-	elif event is InputEventMouseButton: ## botón ratón o rueda
-		match event.button_index:
-			MOUSE_BUTTON_RIGHT: 
-				bdrp = event.pressed 
-				av = false 
-			MOUSE_BUTTON_WHEEL_DOWN: 
-				dz *= 1.05
-			MOUSE_BUTTON_WHEEL_UP:   
-				dz = max( dz/1.05, 0.1 )
-			_: 
-				av = false
-				
-	elif event is InputEventMouseMotion and bdrp: ## movim. ratón
-		dxy += ar * Vector2( -event.relative.x, event.relative.y ) 
 		
-	else: ## otros tipos de eventos (no hace nada)
-		av = false ## (no actualizar transf)
+@onready var ray = $RayCast3D # == nodo `RayCast3D` hijo de la cámara
 
-	if av:
-		_actualiza_transf_vista( )
+func _input( event ):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from = project_ray_origin( mouse_pos )
+	var to = from + project_ray_normal( mouse_pos ) * 1000.0
+	if event is InputEventMouseButton and event.pressed \
+			and event.button_index == MOUSE_BUTTON_LEFT:
+		select_figure_at_mouse(from,to)
+		
+	if event is InputEventMouseMotion and selected_figure:
+		var move = Vector3(event.relative.x * 0.01, 0, event.relative.y * 0.01)
+		selected_figure.position += move
+		
+		
+func _unhandled_key_input(key_event):
+	# Cambiar parte con flechas
+	if key_event.keycode == KEY_UP and not key_event.pressed:
+		current_figure_index = (current_figure_index + 1) % figures.size()
+		select_figure_by_name(figures[current_figure_index])
+		print("Parte: ", figures[current_figure_index])
+	
+	if key_event.keycode == KEY_DOWN and not key_event.pressed:
+		current_figure_index = (current_figure_index - 1 + figures.size()) % figures.size()
+		select_figure_by_name(figures[current_figure_index])
+		print("Parte: ", figures[current_figure_index])
+
+func crear_cubo_en( pos ):
+	var nuevo_cubo = MeshInstance3D.new()
+	nuevo_cubo.mesh = BoxMesh.new()
+	# Elevarlo para que no atraviese el suelo
+	nuevo_cubo.position = pos + Vector3.UP * 0.5
+	var mat = StandardMaterial3D.new()
+	mat.albedo_color = Color(randf(), randf(), randf())
+	nuevo_cubo.material_override = mat
+	get_tree().get_current_scene().add_child(nuevo_cubo)
+	
+	
+## ---------------------------------------------- ##
+## Funcion que selecciona la figura pasada como argumento
+##	figure : figura pasada como argumento
+
+func select_figure(figure):
+	# Seleccionar nuevo
+	selected_figure = figure
+	if selected_figure:
+		print("Seleccionado: ", selected_figure.name)
+
+## ---------------------------------------------- ##
+## Funcion que selecciona el area de la mesh instance pasada como argumento
+##	name : nombre de la mesh instance
+func select_figure_by_name(name):
+	var root = get_tree().get_root().get_child(0)  # Nodo raíz de escena
+	var node = root.find_child(name, true, false)
+	if node:
+		select_figure(node)
+	else:
+		print("ERROR: No se encontró figura: ", name)
+
+	
+## ---------------------------------------------- ##
+## Funcion que selecciona el area de la mesh instance clicada con el raton
+##	name : nombre de la mesh instance
+func select_figure_at_mouse(from, to):
+	print("=== RAYCAST DEBUG ===")
+	print("From: ", from)
+	print("To: ", to)
+	
+	ray.global_position = from
+	ray.look_at(to)
+	ray.target_position = Vector3(0, 0, -1000)
+	ray.force_raycast_update()
+	
+	print("Ray enabled: ", ray.enabled)
+	print("Ray colliding: ", ray.is_colliding())
+	
+	if ray.is_colliding():
+		var figura = ray.get_collider().get_parent()
+		select_figure(figura)
+	else:
+		print("NO HAY COLISIÓN")
---
+++ Cuarto_Curso/Primer_Cuatri/IG/p5/p-5/Scripts/collision_shape_3d_plano.gd
@@ -0,0 +1,1 @@
+extends CollisionShape3D
