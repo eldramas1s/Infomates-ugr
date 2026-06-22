3a0224ac318442e3e974b8d0955fad5858572873
el_dramas <lucashidalgoherrera@gmail.com>
2026-06-22 11:44:25 +0200

    updating

--- Cuarto_Curso/Segundo_Cuatri/Programacion_Web/Practicas/SimulacroVideojuegos/model/DataObject.php
+++ Cuarto_Curso/Segundo_Cuatri/Programacion_Web/Practicas/SimulacroVideojuegos/model/DataObject.php
@@ -5,6 +5,11 @@ require_once __DIR__.'/config.php';
 abstract class DataObject{
     protected $data = array();
 
+    public function __construct( $datos ) {
+        foreach ( $datos as $clave => $valor )
+            if ( array_key_exists( $clave, $this->data ) ) $this->data[$clave] = $valor;
+    }
+
     static public function connect(){
         try{
             $conn = new PDO(DB_DSN,DB_USER,DB_PASS,[
